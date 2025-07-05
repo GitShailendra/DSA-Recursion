885a881c411d91cd01ccfd7609aa12a2475a71b7
GitShailendra <kumarshailrndra7355@gmail.com>
2025-07-05 20:09:57 +0530

    Code Pushed 5 July V2

--- CountNumberDigit.java
+++ CountNumberDigit.java
@@ -1,6 +1,7 @@
 public class CountNumberDigit {
     public static void main(String[] args) {
         int num = 1234;
+        // System.out.println(14/10);
         int result = countNumDigit(num);
         System.out.println(result);
     }
